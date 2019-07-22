771f6da3a0deb22505ec8f2eebac0a14ba92eb79
Dan Schultz <travelguy90@gmail.com>
2019-07-21 19:24:48 -0700

    prepped project for heroku deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 const goalsRoute = require('./routes/goals');
 const usersRoute = require('./routes/users');
@@ -8,6 +9,7 @@ const settingsRoute = require('./routes/settings');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 const uri = process.env.MONGO_URI;
 
@@ -23,4 +25,7 @@ app.use("/auth", authRoute)
 app.use("/settings", settingsRoute);
 
 const port = process.env.PORT || 5000;
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port, () => console.log(`Server running on port ${port}`));
